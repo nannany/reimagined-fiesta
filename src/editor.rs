504d9afbc210bef061854671caa1a26bc2e62b51
minami.yoshihiko <nannany@st.inc>
2024-03-30 08:40:39 +0900

    tilda

--- src/editor.rs
+++ src/editor.rs
@@ -23,16 +23,18 @@ impl Editor {
                 die(error);
             }
         }
-
     }
     pub fn default() -> Self {
-        Self {should_quit: false}
+        Self { should_quit: false }
     }
 
-    fn refresh_screen(&self) -> Result<(), io::Error>  {
+    fn refresh_screen(&self) -> Result<(), io::Error> {
         print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
         if self.should_quit {
             println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            print!("{}", termion::cursor::Goto(1, 1))
         }
         io::stdout().flush()
     }
@@ -44,6 +46,12 @@ impl Editor {
             _ => (),
         }
         Ok(())
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~\r")
+        }
     }
 }
 
