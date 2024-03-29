d17d3cdee94351f1358a732a5591f230099cfd07
minami.yoshihiko <nannany@st.inc>
2024-03-30 08:36:59 +0900

    clear the screen on exit

--- src/editor.rs
+++ src/editor.rs
@@ -31,6 +31,9 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), io::Error>  {
         print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            println!("Goodbye.\r");
+        }
         io::stdout().flush()
     }
 
@@ -53,5 +56,6 @@ fn read_key() -> Result<Key, io::Error> {
 }
 
 fn die(e: io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e);
 }
